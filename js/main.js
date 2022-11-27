844cc62d01e87ad9400b10605d26162bd3234839
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-11-27 19:18:30 -0300

    Implement initial game logic

--- js/main.js
+++ js/main.js
@@ -5,4 +5,35 @@ function getComputerChoice() {
     return options[randomIndex];
 }
 
-console.log(getComputerChoice());
+function getPlayerSelection() {
+    /* Take rock, papaer, scissors input from input and return it */
+    return 0;
+}
+
+function playRound(playerSelection, computerSelection) {
+    /* Given player and computer selections, performs logic to
+    decide winner */
+
+    /* Base Cases: 
+        -rock beats scissors 
+        -scissors beats paper
+        -paper beats rock */
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') 
+        || (playerSelection == 'scissors' && computerSelection == 'paper')
+        || (playerSelection == 'paper' && computerSelection) == 'rock'){
+        // win scenario
+        return `You won :D ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == computerSelection){
+        // draw scenario
+        return 'Draw :/';
+    } else {
+        // lose scenario
+        return `You lose :( ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const player = 'paper';
+const computer = getComputerChoice();
+
+console.log(playRound(player, computer));
